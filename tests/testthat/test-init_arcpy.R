1c1c2d52c097d5cbf3ed3ccfb228b3a785f4a7d4
HarunCelikOtto <hcelik@iastate.edu>
2023-04-01 13:24:04 -0500

    Amend and append tests for additional coverage of init_arcpy().

--- tests/testthat/test-init_arcpy.R
+++ tests/testthat/test-init_arcpy.R
@@ -1,15 +1,48 @@
-test_that("arcgisbinding is loaded", {
-  loaded_packages <- "arcgisbinding" %in% (.packages(all.available = TRUE))
-  expect_equal(loaded_packages, TRUE)
-})
+# ------------------------------------------------------------------------------
+## Area for function return testing
 
-test_that("arc.check_product() returns some license", {
+test_that("arc.check_product() returns a license", {
   check_product <- arc.check_product()
   expect_identical(class(arc.check_product()), class(check_product))
 })
 
-#test_that("An environment path is specified", {})
+test_that("Specified conda_env exists", {
+  all_condas <- conda_list()
+  all_condas_name <- all_condas$name
+  first_conda_env_test <- all_condas_name[1]
 
-#test_that("Only one environment path is specified", {})
+  expect_no_error(init_arcpy(conda_env = first_conda_env_test))
+})
 
-#test_that("An error is given if path(s) is incorrect" {})
+test_that("Specified python file path exists", {
+  expect_error(init_arcpy(python_env_path = "random\\path\\python.exe"))
+})
+
+test_that("Correct python path successfully executes", {
+  all_pythons <- conda_list()
+  all_pythons_paths <- all_pythons$python
+  first_python_path_test <- all_pythons_paths[1]
+
+  expect_no_error(init_arcpy(python_env_path = first_python_path_test))
+})
+
+test_that("Either a python_env_path or conda_env is specified", {
+  expect_error(init_arcpy(conda_env = "hi", python_env_path = "hello there"))
+})
+
+test_that("Both conda_env and python_env_path have to be characters", {
+  expect_error(init_arcpy(python_env_path = 2))
+  expect_error(init_arcpy(conda_env = 1))
+})
+
+test_that("If no argument is provided, an error occurs", {
+  expect_error(init_arcpy())
+})
+
+
+# ------------------------------------------------------------------------------
+## Area for cli testing
+
+# That progress bars work.
+
+# That messages, aborts and alerts work.
